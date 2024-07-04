18126ab789fc1c967d24d1825d08d68d3ec837c1
shamseer8877 <alishamseer@gmail.com>
2024-07-04 14:55:56 +0530

    Pagination style implemented

--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import "./Women.css";
 import TopNavbar from "../components/TopNavbar";
 import BottomNavbar from "../components/BottomNavbar";
+import { Pagination } from "@mui/material";
 
 const Men = () => {
   return (
     <>
       <TopNavbar />
       <BottomNavbar />
+      <div className="pagination-div">
+        <Pagination count={10} variant="outlined" color="primary" />
+      </div>
     </>
   );
 };
--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import "./Women.css";
 import TopNavbar from "../components/TopNavbar";
 import BottomNavbar from "../components/BottomNavbar";
+import { Pagination } from "@mui/material";
 
 const Women = () => {
   return (
     <>
       <TopNavbar />
       <BottomNavbar />
+      <div className="pagination-div">
+        <Pagination count={10} variant="outlined" color="primary" />
+      </div>
     </>
   );
 };
